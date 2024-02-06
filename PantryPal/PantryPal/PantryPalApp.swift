15128f7671fd7c46a81b2742a6964f325dc29ab3
Melissa Munoz <munozme@sheridancollege.ca>
2024-02-06 15:22:55 -0500

    Initial Commit

---
+++ PantryPal/PantryPal/PantryPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryPalApp.swift
+//  PantryPal
+//
+//  Created by Eli Munoz on 2024-02-06.
+//
+
+import SwiftUI
+
+@main
+struct PantryPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
