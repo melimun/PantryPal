4b44a360b23be06b72eeb41b9d775e3e3db769d7
Eli <95142071+melimun@users.noreply.github.com>
2024-04-01 13:34:54 -0400

    Added Recipe details page

--- PantryPal/PantryPal/Models/Recipe.swift
+++ PantryPal/PantryPal/Models/Recipe.swift
@@ -1,4 +1,3 @@
-// Melissa Munoz / Eli - 991642239
 
 
 import Foundation
@@ -26,6 +25,60 @@ struct Recipe: Codable, Identifiable {
     let strMeal: String
     let strMealThumb: String
     
+    let strCategory: String?
+    let strArea: String?
+    let strInstructions: String?
+    let strTags: String?
+    let strYoutube: String?
+    let strSource: String?
+    let strImageSource: String?
+    let strCreativeCommonsConfirmed: String?
+    let dateModified: String?
+    
+    let ingredients: [FoodItem]
+    
+    var strIngredient1: String?
+    var strIngredient2: String?
+    var strIngredient3: String?
+    var strIngredient4: String?
+    var strIngredient5: String?
+    var strIngredient6: String?
+    var strIngredient7: String?
+    var strIngredient8: String?
+    var strIngredient9: String?
+    var strIngredient10: String?
+    var strIngredient11: String?
+    var strIngredient12: String?
+    var strIngredient13: String?
+    var strIngredient14: String?
+    var strIngredient15: String?
+    var strIngredient16: String?
+    var strIngredient17: String?
+    var strIngredient18: String?
+    var strIngredient19: String?
+    var strIngredient20: String?
+    
+    var strMeasure1: String?
+    var strMeasure2: String?
+    var strMeasure3: String?
+    var strMeasure4: String?
+    var strMeasure5: String?
+    var strMeasure6: String?
+    var strMeasure7: String?
+    var strMeasure8: String?
+    var strMeasure9: String?
+    var strMeasure10: String?
+    var strMeasure11: String?
+    var strMeasure12: String?
+    var strMeasure13: String?
+    var strMeasure14: String?
+    var strMeasure15: String?
+    var strMeasure16: String?
+    var strMeasure17: String?
+    var strMeasure18: String?
+    var strMeasure19: String?
+    var strMeasure20: String?
+    
     var id: String {
         idMeal
     }
@@ -34,18 +87,129 @@ struct Recipe: Codable, Identifiable {
         case idMeal
         case strMeal
         case strMealThumb
+        case strCategory, strArea, strInstructions, strTags, strYoutube, strSource, strImageSource, strCreativeCommonsConfirmed, dateModified
+        
+        case strIngredient1
+        case strIngredient2
+        case strIngredient3
+        case strIngredient4
+        case strIngredient5
+        case strIngredient6
+        case strIngredient7
+        case strIngredient8
+        case strIngredient9
+        case strIngredient10
+        case strIngredient11
+        case strIngredient12
+        case strIngredient13
+        case strIngredient14
+        case strIngredient15
+        case strIngredient16
+        case strIngredient17
+        case strIngredient18
+        case strIngredient19
+        case strIngredient20
+        
+        case strMeasure1
+        case strMeasure2
+        case strMeasure3
+        case strMeasure4
+        case strMeasure5
+        case strMeasure6
+        case strMeasure7
+        case strMeasure8
+        case strMeasure9
+        case strMeasure10
+        case strMeasure11
+        case strMeasure12
+        case strMeasure13
+        case strMeasure14
+        case strMeasure15
+        case strMeasure16
+        case strMeasure17
+        case strMeasure18
+        case strMeasure19
+        case strMeasure20
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        //For get by Ingredients
         self.idMeal = try container.decodeIfPresent(String.self, forKey: .idMeal) ?? "NA"
         self.strMeal = try container.decodeIfPresent(String.self, forKey: .strMeal) ?? "NA"
         self.strMealThumb = try container.decodeIfPresent(String.self, forKey: .strMealThumb) ?? "NA"
+        
+        //For get Recipe Details by ID
+        self.strCategory = try container.decodeIfPresent(String.self, forKey: .strCategory)
+        self.strArea = try container.decodeIfPresent(String.self, forKey: .strArea)
+        self.strInstructions = try container.decodeIfPresent(String.self, forKey: .strInstructions)
+        self.strTags = try container.decodeIfPresent(String.self, forKey: .strTags)
+        self.strYoutube = try container.decodeIfPresent(String.self, forKey: .strYoutube)
+        self.strSource = try container.decodeIfPresent(String.self, forKey: .strSource)
+        self.strImageSource = try container.decodeIfPresent(String.self, forKey: .strImageSource)
+        self.strCreativeCommonsConfirmed = try container.decodeIfPresent(String.self, forKey: .strCreativeCommonsConfirmed)
+        self.dateModified = try container.decodeIfPresent(String.self, forKey: .dateModified)
+        
+        
+        //ingredients
+        // Initialize ingredients array
+        var ingredients: [FoodItem] = []
+        
+        // Loop over ingredient keys and append non-empty values to the ingredients array
+        for index in 1...20 {
+            if let ingredientKey = CodingKeys(rawValue: "strIngredient\(index)"),
+               let measurementKey = CodingKeys(rawValue: "strMeasure\(index)"),
+               let ingredient = try container.decodeIfPresent(String.self, forKey: ingredientKey),
+               let measurement = try container.decodeIfPresent(String.self, forKey: measurementKey),
+               !ingredient.isEmpty, !measurement.isEmpty {
+                ingredients.append(FoodItem(name: ingredient,measure: measurement))
+            }
+        }
+        
+        self.ingredients = ingredients
     }
     
     init(){
         self.idMeal = ""
         self.strMeal = ""
         self.strMealThumb = ""
+        self.strCategory = ""
+        self.strArea = ""
+        self.strInstructions = ""
+        self.strTags = ""
+        self.strYoutube = ""
+        self.strSource = ""
+        self.strImageSource = ""
+        self.strCreativeCommonsConfirmed = ""
+        self.dateModified = ""
+        self.ingredients = [FoodItem]()
+    }
+}
+
+struct FoodItem: Codable, Hashable {
+    let name: String
+    let measure: String
+    
+    init(name: String, measure: String) {
+        self.name = name
+        self.measure = measure
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case measure
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? "NA"
+        self.measure = try container.decodeIfPresent(String.self, forKey: .measure) ?? "NA"
+    }
+    
+    init(){
+        self.name = ""
+        self.measure = ""
     }
 }
--- PantryPal/PantryPal/Views/RecipeViews/RecipeDetails.swift
+++ PantryPal/PantryPal/Views/RecipeViews/RecipeDetails.swift
@@ -8,61 +8,125 @@
 import SwiftUI
 
 struct RecipeDetails: View {
+    
+    @EnvironmentObject private var recipeManager: RecipeManager
+    @State var recipeID: String
+    
+    
     var body: some View {
         
-        VStack{
-            
-            Text("Recipe Name").font(.title).fontWeight(.bold)
-            Text("Tags: Breakfast, Balanced, 500 calories").font(.subheadline)
-            
-            AsyncImage(url: URL(string: "https://placehold.co/500x250/png")).padding()
+        ScrollView{
             VStack{
                 
-                HStack{
-                    
-                    
-                    Button(action: {
-                        //
-                    }) {
-                        Image(systemName: "heart.fill")
-                            .foregroundColor(.white)
-                        Text("Favourite").foregroundColor(.white)
-                    }.tint(.red).buttonStyle(.borderedProminent)
-                        .padding([.trailing], 50)
-                    
-                    Button(action: {
-                        //
-                    }) {
-                        Image(systemName: "cart.fill")
-                            .foregroundColor(.white)
-                        Text("Buy").foregroundColor(.white)
-                    }.tint(.blue).buttonStyle(.borderedProminent)
-                    
-                }
+                var recipe = self.recipeManager.recipe.meals.first
                 
-                Section(header:Text("Ingredients:").bold()){
-                    
-                    Text("1 Cabbage")
-                    Text("2 Carrots")
-                    Text("3 Chicken Legs")
-                }.frame(maxWidth: .infinity, alignment: .leading)
+                AsyncImage(
+                    url: URL(string:recipe?.strMealThumb ?? ""),
+                    content: { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(10)
+                    },
+                    placeholder: {
+                        ProgressView()
+                    }
+                )
                 
-                HStack{Spacer()}
                 
-                Section(header:Text("Steps:").bold()){
+                
+                VStack{
                     
-                    Text("Step 1: Prepare the ingredients.")
-                    Text("Step 2: Turn on the stovetop to medium heat.")
-                    Text("Step 3: (...)")
-                }.frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                    
+                    //Title
+                    Text("\(recipe?.strMeal ?? "No Title")").font(.title).fontWeight(.bold)
+                    
+                    //Tags
+                    Text("Tags: \(recipe?.strTags ?? "No Tags"), \(recipe?.strArea ?? "No Area"), \(recipe?.strCategory ?? "No Category")").font(.subheadline)
+                    
+                    
+                    HStack{
+                        
+                        if let sourceURLString1 = recipe?.strYoutube, let sourceURL1 = URL(string: sourceURLString1) {
+                            Link("Youtube Tutorial", destination: sourceURL1)
+                        } else {
+                            Text("No Link Available")
+                        }
+                        
+                        Text(" | ")
+                        
+                        
+                        if let sourceURLString2 = recipe?.strSource, let sourceURL2 = URL(string: sourceURLString2) {
+                            Link("Original Source", destination: sourceURL2)
+                        } else {
+                            Text("No Link Available")
+                        }
+                    }//HStack
+                    
+                    Spacer()
+                    
+                    HStack{
+                                                
+                        Button(action: {
+                            //Favourite
+                        }) {
+                            Image(systemName: "heart.fill")
+                                .foregroundColor(.white)
+                            Text("Favourite").foregroundColor(.white)
+                        }.tint(.red).buttonStyle(.borderedProminent)
+                            .padding([.trailing], 50)
+                        
+                        Button(action: {
+                            //Buy Ingredients
+                        }) {
+                            Image(systemName: "cart.fill")
+                                .foregroundColor(.white)
+                            Text("Buy").foregroundColor(.white)
+                        }.tint(.blue).buttonStyle(.borderedProminent)
+                        
+                    }.padding()
+                    
+                    //Ingredients
+                    Section(header: Text("Ingredients:").bold()) {
+                        if let recipe = recipe {
+                            ForEach(recipe.ingredients ?? [], id: \.self) { ingredient in
+                                Text("\(ingredient.name) - \(ingredient.measure)")
+                                    .font(.subheadline)
+                            }
+                        }
+                    }.padding(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    HStack{Spacer()}
+                    
+                    //Recipes
+                    Section(header:Text("Steps:").bold()){
+                        
+                        Text(recipe?.strInstructions?.replacingOccurrences(of: ".", with: ".\n\n") ?? "No Instructions")
+                            .font(.subheadline)
+
+                    }.padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }//VStack
+                .background(Color(.white))
+                .cornerRadius(20)
+                .padding()
             }//VStack
-            .padding()
-        }//VStack
-        
+            .background(Gradient(colors: [.teal, .cyan, .green]).opacity(0.6))
+            .onAppear() {
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    
+                    self.recipeManager.getRecipeByID(recipeID: self.recipeID)
+                }
+            }//onAppear
+        }//ScrollView
         Spacer()
     }//body
 }//view
 
+
 #Preview {
-    RecipeDetails()
+    RecipeDetails(recipeID: "")
 }
+
