d212594f789e0ea6097aa3d916ddef5e5579fc58
jaffuu <mackechr@sheridancollege.ca>
2024-04-12 09:44:52 -0400

    Started MapView and firebase functions, WalmartAPI not available so TempModel will be used instead

--- PantryPal/PantryPal/Views/ItemViews/MapView.swift
+++ PantryPal/PantryPal/Views/ItemViews/MapView.swift
@@ -7,12 +7,109 @@
 
 import SwiftUI
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import SwiftUI
+import MapKit
 
-#Preview {
-    MapView()
-}
+
+struct MapView: View {
+    
+    //for tabview
+   // @EnvironmentObject var router : TabRouter
+    @EnvironmentObject var locationHelper : LocationHelper
+    
+    @EnvironmentObject var fireDBHelper : FireDBHelper
+    
+    @State private var search: String = ""
+    
+    @State private var locations: [Location] = [Location]()
+    
+    @State private var isSheetPresented: Bool = false
+    
+    
+    var body: some View {
+        ZStack{
+            VStack{
+                
+                ZStack(alignment: .top) {
+                    
+                    WalmartMap(locations: locations).environmentObject(self.locationHelper)
+                        .sheet(isPresented: $isSheetPresented) {
+                            SheetView(locations: self.locations)
+                        }//sheet
+                    
+                    TextField("Search For Nearby Locations", text: $search, onEditingChanged: { _ in })
+                    {
+                        // commit
+                        self.getNearByLocations(search:self.search)
+                    }.textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        .offset(y: 44)
+                }
+            }//VStack
+            
+                //                self.fireDBHelper.retrieveAllLocations()
+                //                self.getFavouriteLocations()
+                
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    
+                    Button(action: {
+                        self.refreshMap()
+                    }) {
+                        Image(systemName: "arrow.clockwise.circle.fill")
+                            .foregroundColor(.green)
+                    }
+                    
+                }//ToolbaritemGroup
+            }
+        }//ZStack
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.yellow)
+        .clipped()
+    }//body
+    
+    private func refreshMap() {
+        self.locations = []
+        self.search = ""
+        self.getNearByLocations(search: self.search)
+    }
+    
+    private func getNearByLocations(search : String) {
+        
+        //MkLocalSearch vs Geocoding
+        //Geocoding recommendations based off coordinations, but userQuery, Mklocalsearch is more common to use
+        //A utility object for initiating map-based searches and processing the results.
+        
+        DispatchQueue.main.async {
+            
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = search
+            
+            let search = MKLocalSearch(request: request)
+            search.start { (response, error) in
+                if let response = response {
+                    
+                    let mapItems = response.mapItems
+                    
+                    self.locations = mapItems.map {
+                        Location(placemark: $0.placemark)
+                        
+                    }//self.locations
+                    
+                    self.isSheetPresented = true
+                    
+                }
+            }//if/else
+        }//search.start
+        
+    }//getNearBy
+    
+    
+    
+    
+}//Struct
+
+//#Preview {
+//    MapView().environmentObject(TabRouter())
+//}
--- PantryPal/PantryPal/Views/ItemViews/SheetView.swift
+++ PantryPal/PantryPal/Views/ItemViews/SheetView.swift
@@ -6,13 +6,69 @@
 //
 
 import SwiftUI
+import MapKit
+
 
 struct SheetView: View {
+    @State var locations: [Location]
+    @State private var showAlert: Bool = false
+    @State private var selectedLocation: Location?
+    @EnvironmentObject var firebaseHelper : FireDBHelper
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            
+            
+            List {
+                
+                ForEach(self.locations, id: \.id) {
+                    
+                    location in
+                    
+                    Button{
+                        
+                        print(#function, "Clicking \(location.name)")
+                        self.showAlert = true
+                        self.selectedLocation = location
+                        
+                    }label:{
+                        
+                        HStack(alignment: .lastTextBaseline){
+                            
+                            
+                            
+                            VStack(alignment: .leading) {
+                                Text(location.name)
+                                    .fontWeight(.bold)
+                                
+                                Text(location.title)
+                                    .foregroundColor(.black)
+                            }//VStack
+                        }//HStack
+                        .padding()
+                    }//Button
+                    
+                }//ForEach
+                
+            }//List
+            .listStyle(.insetGrouped)
+        }//VStack
+       
     }
+    
+    
 }
 
-#Preview {
-    SheetView()
+struct TextFieldGrayBackgroundColor: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(12)
+            .background(.gray.opacity(0.1))
+            .cornerRadius(8)
+            .foregroundColor(.primary)
+    }
+    
 }
+
