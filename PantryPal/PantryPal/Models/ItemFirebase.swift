3aa8b20dafcc0bcb109bf40f195e19f75a3d672e
jaffuu <mackechr@sheridancollege.ca>
2024-04-12 09:44:52 -0400

    Started MapView and firebase functions, WalmartAPI not available so TempModel will be used instead

--- PantryPal/PantryPal/Models/ItemFirebase.swift
+++ PantryPal/PantryPal/Models/ItemFirebase.swift
@@ -12,4 +12,38 @@ struct ItemFirebase : Codable, Hashable{
     
     @DocumentID var id : String? = UUID().uuidString
     
+    var itemName : String = ""
+    var category : String = ""
+    var inStock : Bool = false
+    
+    init(){
+        self.itemName = ""
+        self.category = ""
+        self.inStock = false
+    }
+    
+    init(itemName : String, category : String, inStock : Bool){
+        self.itemName = itemName
+        self.category = category
+        self.inStock = inStock
+    }
+    
+    init?(dictionary: [String: Any]){
+        guard let itemName = dictionary["itemName"] as? String else{
+            return nil
+        }
+        
+        guard let category = dictionary["category"] as? String else{
+            return nil
+        }
+        
+        guard let inStock = dictionary["inStock"] as? Bool else{
+            return nil
+        }
+        
+        
+        
+        self.init(itemName: itemName, category: category, inStock: inStock)
+        
+    }
 }
